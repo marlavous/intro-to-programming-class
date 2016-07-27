288f81d3a6061d50614d3fdd687897a27d6f7606
marlavous <marlavous@sbcglobal.net>
2016-07-26 21:17:41 -0700

    awesome calculator

---
+++ calculator.py
@@ -0,0 +1,49 @@
+def add(num1, num2):
+	return num1 + num2
+
+
+def subtract(num1, num2):
+	return num1 - num2
+
+
+def multiply(num1, num2):
+	return num1 * num2
+
+
+def divide(num1, num2):
+	return num1/num2
+
+def modulo(num1, num2):
+	return num1%num2
+
+def power (base, exponent):
+	return base ** exponent
+
+def square(num):
+	return num ** 2
+
+print add(4,5) + subtract(9,6)
+
+print divide(12,2) - 60
+
+print add(1,2) + 3
+
+print square(add(1,2))
+
+print divide(modulo(3,4), multiply(9,9))
+
+print multiply(7, add(3,8))
+
+print multiply((subtract(add(1,2), 3)), add(4,5))
+
+print power(3,add(2,3))
+
+# print add(4,5)
+# print subtract(5,4)
+# print multiply(2,2)
+# print divide(4,2)
+# print modulo(10,2)
+# print power(2,3)
+# print square(4)
+
+

b628af6e7cdbf31e162ea3913d2b9922eee34f8d
Marla Marcianelli <marlavous@users.noreply.github.com>
2016-08-04 18:46:07 -0700

    Add files via upload

--- shoppinglist.py
+++ shoppinglist.py
@@ -1,54 +1,56 @@
-shoppinglist =[]
+shoppinglist =["apples", "cheese", "catfood"]
 
-def alphabetize():
-	shoppinglist.sort()
-	return shoppinglist
 
 def additem(item):
-	item = item.lower()
-	if item != shoppinglist:
-		item.append(shoppinglist)
-		print "s% has been added to your shopping list" % (item)
+	if item.lower != shoppinglist:
+		shoppinglist.append(item)
+		shoppinglist.sort()
+		print "%s has been added to your shopping list" % (item)
 
 	else:
-		print "That is already on the list"
+		print "%s already on the list" % (item)
 
 	return shoppinglist
 
 def removeitem(item):
-	item = item.lower()
-	if item == shoppinglist:
-		item.remove(item)
-		print "s% has been removed from your shopping list" % (item)
+	if item.lower == shoppinglist:
+		shoppinglist.remove(item)
+		shoppinglist.sort()
+		print "%s has been removed from your shopping list" % (item)
 
 	else:
-		print "s% was never on the list" % (item)
+		print "%s was never on the list" % (item)
 
 
 def main():
 	print "Lets make a shopping list!"
 	print "Would you like to add something?"
-	choice = str(raw_input())
 
-	if choice == "yes":
-		print "What would you like to add?"
-		item = str(raw_input())
-		return additem(shoppinglist)
-		return alphabetize(shoppinglist)
-		print shoppinglist
+	print additem ("bread")
+	print additem ("meat")
+	print additem ("apples")
+	print shoppinglist
+	print removeitem ("apples")
 
-	elif choice == "no":
-		print "Would you like to remove something from your shopping list?"
-		choice == str(raw_input())
+	# choice = str(raw_input())
 
-		if choice == "yes":
-			print "what would you like to remove?"
-			item = str(raw_input())
-			return removeitem()
-			return alphabetize()
-			print shoppinglist
+	# if choice == "yes":
+	# 	print "What would you like to add?"
+	# 	item = str(raw_input())
+	# 	return additem(shoppinglist)
+	# 	print shoppinglist
 
+	# elif choice == "no":
+	# 	print "Would you like to remove something from your shopping list?"
+	# 	choice == str(raw_input())
 
+	# 	if choice == "yes":
+	# 		print "what would you like to remove?"
+	# 		item = str(raw_input())
+	# 		return removeitem()
+	# 		print shoppinglist
+
+	
 
 if __name__ == '__main__':
 	main()
